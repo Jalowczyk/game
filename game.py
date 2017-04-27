130070ee37bba0602716a5f64aa2c7ab16d37f90
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-04-27 12:38:35 +0200

    @ is movinggit add game.py!

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import sys, tty, termios, os
+
 def handle_user_decision():
     user_decision_height = int(input("Pick height: "))
     user_decision_height += 2
@@ -33,13 +35,13 @@ def create_board(width, height):
 
 
 def print_board(board):
-
+    os. system("clear")
     for row in board:
         print(*row)
 
 
 def getch():
-    import sys, tty, termios
+
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
@@ -52,7 +54,6 @@ def getch():
 
 def insert_player(board, y, x):
     player = "@"
-
     board[y][x] = "@"
 
     return board
@@ -60,15 +61,37 @@ def insert_player(board, y, x):
 
 def main():
 
-    width, height = handle_user_decision()
     x_hero = 1
     y_hero = 1
     board = create_board(width, height)
     insert_player(board, y_hero, x_hero)
     print_board(board)
+    x = getch()
+
+    while x != "q":
+
+
+        x = getch()
+        if x == "d":
+            x_hero += 1
+        elif x == "a":
+            x_hero -= 1
+        elif x == "s":
+            y_hero += 1
+        elif x == "s":
+            y_hero -= 1
+        elif x == "q":
+            sys.exit()
+
+        board = create_board(width, height)
+        insert_player(board, y_hero, x_hero)
+        print_board(board)
+
+
+
+
+
+width, height = handle_user_decision()
+
 
 main()
-
-while True:
-    x = getch()
-    print(x)
