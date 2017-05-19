9d2cef586f1af641c520ca9fe162f4cd40729e60
Jalowczyk <probulska.jagoda@gmail.com>
2017-05-19 09:16:17 +0200

    Update interactions.py
    more functions

--- interactions.py
+++ interactions.py
@@ -9,3 +9,15 @@ def is_door_closed(obstacles, door):
 
 def are_all_twanas_in_inventory(inventory):
     return inventory["twanas"] == 9
+
+def is_taking_item(position_on_board, item_place, item, inventory):
+    return is_touching(position_on_board, item_place) and not is_item_in_inventory(item, inventory)
+
+def are_items_in_inventory(first_item, second_item, inventory):
+    return is_item_in_inventory(first_item, inventory) and is_item_in_inventory(second_item, inventory)
+
+def is_item_and_all_twanas_in_inventory(item, inventory):
+    return is_item_in_inventory(item, inventory) and are_all_twanas_in_inventory(inventory)
+
+def is_entering_door(position_on_board, item_place, item, inventory):
+    return is_touching(position_on_board, item_place) and not is_item_in_inventory(item, inventory)
